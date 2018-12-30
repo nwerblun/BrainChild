be1c8fa580e27f18ad16af55dffb82211645a0f2
Nick Werblun <nickwerblun@gmail.com>
2018-12-30 00:09:18 -0800

    rotation semi working

---
+++ Assets/Scripts/TracksMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracksMouse : MonoBehaviour
+{
+    void Update() {
+        Vector2 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 currPos = transform.position;
+        Vector2 diff = currMousePos - currPos;
+
+        float currAngle = transform.rotation.eulerAngles.z;
+        float mouseAngle = (Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg);
+
+        Vector3 desiredRotation = new Vector3(0, 0, Mathf.LerpAngle(currAngle, mouseAngle, Time.deltaTime * 50));        
+        //Debug.Log(angleDiff);
+        transform.eulerAngles = desiredRotation;
+    }
+}
--- Assets/Scripts/WeaponInfo.cs
+++ Assets/Scripts/WeaponInfo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WeaponInfo : MonoBehaviour
 {
 /* Has a bunch of variables
  * base damage
