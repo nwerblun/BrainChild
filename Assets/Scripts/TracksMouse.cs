ca2f07bfdac8be914ee24e86bf298141bf9b07ad
Nick Werblun <nickwerblun@gmail.com>
2019-01-19 22:32:55 -0800

    stuff

--- Assets/Scripts/TracksMouse.cs
+++ Assets/Scripts/TracksMouse.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class TracksMouse : MonoBehaviour
 {
-    private Vector3 originalScale;
-    public bool flipOnQuandrantChange;
     public Transform defaultTrack;
 
     private void Start()
     {
-        originalScale = transform.localScale;
     }
 
     public void UpdateObj() {
@@ -22,12 +19,6 @@ public class TracksMouse : MonoBehaviour
             float currAngle = transform.rotation.eulerAngles.z;
             float mouseAngle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
             desiredRotation = new Vector3(0, 0, Mathf.LerpAngle(currAngle, mouseAngle, Time.deltaTime * 50));
-            if (flipOnQuandrantChange) {
-                if (mouseAngle < 90 && mouseAngle > -90)
-                    transform.localScale = originalScale;
-                else
-                    transform.localScale = new Vector3(originalScale.x, -originalScale.y, originalScale.z);
-            }
         } else {
             if (defaultTrack != null)
                 desiredRotation = defaultTrack.rotation.eulerAngles;
